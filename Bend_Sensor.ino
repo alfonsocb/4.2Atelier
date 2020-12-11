3225eca29d110abe3cbeba7114281f3c84f6e9fa
alfonsocb <59902759+alfonsocb@users.noreply.github.com>
2020-12-11 15:25:01 -0500

    Update Bend_Sensor.ino

--- Bend_Sensor.ino
+++ Bend_Sensor.ino
@@ -2,14 +2,12 @@ int analogInPin = A5;
 int sensorValue = 0;
 int ledPin = 10 ;
 void setup() {
-  // initialize serial communications at 9600 bps:
   Serial.begin(9600);
  pinMode(A5, INPUT_PULLUP); 
  pinMode(10, OUTPUT); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   // read the analog in value:
   sensorValue = analogRead(analogInPin);              
   // print the results to the serial monitor:
